7663bd142b87840d481f043c8cead16b5c1e1e0b
Pradheon <pradheon@gmail.com>
2022-02-09 14:41:09 -0800

    Initial Commit

---
+++ JoshanRai-ViewsAndModifiers/JoshanRai_ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_ViewsAndModifiersApp.swift
+//  JoshanRai-ViewsAndModifiers
+//
+//  Created by Joshan Rai on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
